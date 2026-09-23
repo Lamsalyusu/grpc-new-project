64591c6b23d5a28706f382af8bca27aafd76c858
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-09-23 23:05:28 +0545

    fix: added user email validation instead of id

--- server/src/validators/collaborationReqvalidation.ts
+++ server/src/validators/collaborationReqvalidation.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import validator from 'validator';
 export const requestSchema = z.object({
     // sender_id:z.uuid("invalid id format"),
-    receiver_id:z.uuid("invalid id format")
+    // receiver_email:z.uuid("invalid id format")
+    receiver_email:z.uuid('invalid email format')
 });
 export type RequestType = z.infer<typeof requestSchema>;
