ebc7d131612a3a801134bf6f6c217feadb64a0d5
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-09-25 22:55:44 +0545

    fix: added receiver email for searching

--- InternProject1/src/validators/collaborationReqvalidation.ts
+++ InternProject1/src/validators/collaborationReqvalidation.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 import validator from 'validator';
 export const requestSchema = z.object({
     // sender_id:z.uuid("invalid id format"),
-    receiver_id:z.uuid("invalid id format")
+    // receiver_id:z.uuid("invalid id format")
+    receiver_email:z.email("Invalid email format")
 });
 
 export type RequestType = z.infer<typeof requestSchema>;
